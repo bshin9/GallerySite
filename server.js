254816f5f53b384229089e5c50a7bdc0197b6a43
Brian Shin <bshin9@yahoo.com>
2019-11-27 13:26:55 -0500

    seeded images

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var express = require("express");
 var mongoose = require("mongoose");
 
@@ -7,13 +8,19 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/imageperformance", {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/imageperformance",
+  {
+    useNewUrlParser: true
+  }
+);
 
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`);
