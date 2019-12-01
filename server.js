22be871bbc09a5469b50cc03d39886232a4b403f
Brian Shin <bshin9@yahoo.com>
2019-12-01 09:44:54 -0500

    fixes

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
